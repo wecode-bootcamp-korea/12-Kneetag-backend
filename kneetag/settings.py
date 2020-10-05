18857c0e56d9f8a053f877fb3b7329b3cc2db6ce
soo <soo@sooui-MacBookPro.local>
2020-10-05 18:53:27 +0900

    ADD : 백엔 초기 세팅 추가 수정 완료

--- kneetag/settings.py
+++ kneetag/settings.py
@@ -112,20 +112,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-
-
-
-
-
-
-
 
 #REMOVE_APPEND_SLASH_WARNING
 APPEND_SLASH = False

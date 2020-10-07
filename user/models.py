6ca11ace4699e40a57abd5b92c73ff8cc908dd48
soo <soo@sooui-MacBookPro.local>
2020-10-07 22:09:27 +0900

    Add : Product model 수정

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     is_active  = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return self.name
     

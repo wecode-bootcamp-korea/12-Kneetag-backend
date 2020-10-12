0d32a6322671859e91fee63633db797e8641612c
DasomJung24 <jeongdasom6@gmail.com>
2020-10-12 16:23:42 +0900

    user.views.py작성

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,6 @@ app_name = 'user'
 urlpatterns = [
     path('/signup', views.SignUpView.as_view()),
     path('/activate', views.ActivateView.as_view()),
+    path('/signin', views.SignInView.as_view()),
+    path('/googlesignin', views.GoogleSignInView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,9 @@
+import json
+import jwt
+import bcrypt
+import datetime
+import requests
+
 from django.views                   import View
 from django.http                    import JsonResponse
 from django.db.models               import Q
@@ -6,14 +12,10 @@ from django.core.validators         import validate_email
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail               import EmailMessage
 from my_settings                    import SECRET, ALGORITHM, EMAIL
+
 from .models                        import User
 from .text                          import message
 from .utils                         import login_decorator
-import json
-import jwt
-import bcrypt
-import datetime
-import requests
 
 class SignUpView(View):
     def post(self, request):
@@ -62,10 +64,50 @@ class SignUpView(View):
 class ActivateView(View):
     @login_decorator
     def get(self, request):
-        user = User.objects.get(id=request.user.id)
+        user = request.user
         user.is_active = True
         user.save()
         expire = datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)
         access_token = jwt.encode({'user_id':user.id, 'exp':expire}, SECRET['secret'], algorithm=ALGORITHM).decode('utf-8')
 
+        return JsonResponse({'Authorization':access_token}, status=200)
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(Q(email=data['user']) | Q(name=data['user'])).exists():
+                user = User.objects.get(Q(email=data['user']) | Q(name=data['user']))
+
+                if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                    return JsonResponse({'message':'WRONG PASSWORD'}, status=400)
+            
+                expire = datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)
+                access_token = jwt.encode({'user_id':user.id, 'exp':expire}, SECRET['secret'], algorithm=ALGORITHM).decode('utf-8')
+
+                return JsonResponse({'Authorization':access_token}, status=200)
+            
+            return JsonResponse({'message':'INVALID REQUEST'}, status=400)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY ERROR'}, status=400)   
+
+class GoogleSignInView(View):
+    def post(self, request):
+        id_token        = request.headers.get('id_token', None)
+        user_request    = requests.get(f'http://oauth2.googleapis.com/tokeninfo?id_token={id_token}')
+        user_infomation = user_request.json()
+        google_email    = user_infomation.get('email')
+        google_name     = user_infomation.get('name')
+        expire          = datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)
+
+        if User.objects.filter(email=google_email).exists():
+            user         = User.objects.get(email=google_email)
+            access_token = jwt.encode({'user_id':user.id,'exp':expire}, SECRET['secret'], algorithm=ALGORITHM).decode('utf-8')
+
+            return JsonResponse({'Authorization':access_token}, status=200)
+        
+        user         = User.objects.create(name=google_name, email=google_email, is_active=True)
+        access_token = jwt.encode({'user_id':user.id,'exp':expire}, SECRET['secret'], algorithm=ALGORITHM).decode('utf-8')
+        
         return JsonResponse({'Authorization':access_token}, status=200)

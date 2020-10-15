8c1285ff60e9234ecbed051b0fb2a38e1b681eb5
DasomJung24 <jeongdasom6@gmail.com>
2020-10-15 11:18:52 +0900

    cart_views.py 작성

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .           import views
 
 app_name = 'cart'
 urlpatterns = [
-    
+    path('', views.CartView.as_view()),
+    path('/<int:cart_id>', views.CartView.as_view()),
 ]
